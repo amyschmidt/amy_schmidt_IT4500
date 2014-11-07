a3e860fb3b494e356e037a2c4de362170b6e0fdf
Amy Schmidt <xaschmidtx@gmail.com>
2014-11-07 17:04:28 -0600

    Page Based HW

--- ImageViewerPractice/ImageViewerPractice/ViewController.swift
+++ ImageViewerPractice/ImageViewerPractice/ViewController.swift
@@ -20,15 +20,71 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         let image = UIImage(named: "babyhusky.png")
         imageView = UIImageView(image:image)
         imageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size:image!.size)
+        
         scrollView.addSubview(imageView)
         scrollView.contentSize = image!.size
         
-        // Do any additional setup after loading the view, typically from a nib.
+        var doubleTapRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.numberOfTouchesRequired = 1
+        scrollView.addGestureRecognizer(doubleTapRecognizer)
+        
+        //may have to fix here because can't use frame 
+        //need something that will tell width and height in auto layout
+        let scrollViewFrame = scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+        let minScale = min(scaleWidth, scaleHeight)
+        scrollView.minimumZoomScale = minScale
+        
+        scrollView.maximumZoomScale = 2.0
+        scrollView.zoomScale = minScale
+        
+        centerScrollViewContents()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func centerScrollViewContents() {
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = imageView.frame
+        
+        if contentsFrame.size.width < boundsSize.width {
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
+        } else {
+            contentsFrame.origin.x = 0.0
+        }
+        
+        if contentsFrame.size.height < boundsSize.height {
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
+        } else {
+            contentsFrame.origin.y = 0.0
+        }
+        
+        imageView.frame = contentsFrame
+    }
+    
+    func scrollViewDoubleTapped(recognizer: UITapGestureRecognizer) {
+        let pointInView = recognizer.locationInView(imageView)
+        
+        var newZoomScale = scrollView.zoomScale * 1.5
+        newZoomScale = min(newZoomScale, scrollView.maximumZoomScale)
+        
+        let scrollViewSize = scrollView.bounds.size
+        let w = scrollViewSize.width / newZoomScale
+        let h = scrollViewSize.height / newZoomScale
+        let x = pointInView.x - (w / 2.0)
+        let y = pointInView.y - (h / 2.0)
+        
+        let rectToZoomTo = CGRectMake(x, y, w, h)
+        scrollView.zoomToRect(rectToZoomTo, animated: true)
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        centerScrollViewContents()
     }
 
 
